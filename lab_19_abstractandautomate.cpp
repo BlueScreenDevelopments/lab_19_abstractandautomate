4fc546ae7bf69fd2ab94939b19bc1031e1e6f221
Brandon J <bsjulao@gmail.com>
2025-10-06 18:26:02 -0700

    Commit - Updating Comments

--- lab_19_abstractandautomate.cpp
+++ lab_19_abstractandautomate.cpp
@@ -4,7 +4,7 @@ using namespace std;
 /*
 Brandon Julao
 COMSC-210-5470
-10.05.25
+10.07.25
 lab-19-abstractandautomate
 */
 
@@ -31,6 +31,7 @@ public:
         head = n;
     }
 
+    //display logic to show each review and compute the average
     void display() {
         cout << "Movie: " << title << endl;
         cout <<"--------------------------" << endl;
@@ -42,6 +43,15 @@ public:
                  << " - " << p->comment << endl;
 
             sum+= p->rating;
+        }
+    }
+
+    //cleans up all review nodes to prevent leaks
+    ~Movie() {
+        while (head) {
+            ReviewNode* temp = head;
+            head = head->next;
+            delete temp;
         }
     }
 
